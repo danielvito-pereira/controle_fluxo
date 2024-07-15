b3975a239e1206ec95ec8deaed951801fb047bb9
danielvito-pereira <danielvitor081@gmail.com>
2024-07-14 21:50:31 -0300

    fim_aula

--- src/SistemaMedida.java
+++ src/SistemaMedida.java
@@ -1,7 +1,7 @@
 public class SistemaMedida {
   public static void main(String[] args) {
     String sigla = "M";
-
+    /* 
     if(sigla == "P"){
       System.out.println("Pequeno");
     }
@@ -14,5 +14,22 @@ public class SistemaMedida {
     else{
       System.out.println("Indefinido");
     }
+      */
+    switch (sigla) {
+      case "P":{
+        System.out.println(" Pequeno");
+        break;
+      }
+      case "M":{
+        System.out.println(" Medio");
+        break;
+      }  
+      case "G":{
+        System.out.println(" Grande");
+        break;
+      }
+      default:
+        System.out.println(" Indefinido");
+    }  
   }
 }
